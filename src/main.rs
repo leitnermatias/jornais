103122a51796e0b9c4c83c24264ba063dbf2d10e
Matias Leitner <leitnermatias22@gmail.com>
2023-08-21 20:48:12 -0300

    Add nosave flag

--- src/main.rs
+++ src/main.rs
@@ -28,7 +28,7 @@ fn menu(title: &str, options: &[&str]) -> String {
 
 }
 
-fn _print_news(title: &str, news: Vec<JournalNew>) {
+fn print_news(title: &str, news: Vec<JournalNew>) {
     println!("{}", title.green());
     let separator = "----------------".bright_white();
 
@@ -75,146 +75,172 @@ async fn save_news_to_database(pool: &Pool<MySql>, news: JournalNew, newspaper_n
 #[tokio::main]
 async fn main() {
 
-    let mut load_file = false;
-    let db_info_fp = std::path::Path::new("db_conn.json");
+    let args: Vec<String> = std::env::args().collect();
 
-    if db_info_fp.exists() {
-        let user_load = menu("There are database connection settings saved, do you want to load them?", &["Yes", "No"]);
+    if args[1] != "nosave" {
 
-        if user_load.as_str() == "0" {
-            println!("{}", "[ Loading information to connect to database from file ]".green());
-            load_file = true;
-        }
-    }
-
-
-    let mut db_info = DBInfo {
-        user: String::from(""),
-        password: String::from(""),
-        name: String::from(""),
-        port: String::from("")
-    };
-
-    if !load_file {
-        db_info.user = menu("Enter your MySQL username", &[]);
-        db_info.password = menu("Enter your MySQL password", &[]);
-        db_info.name = menu("Enter the name of the database you want to use", &[]);
-        db_info.port = menu("Enter the port where the database server is running", &[]);
-
-        let save_to_file = menu("Do you want to save these connection settings to a file? (This will remove previously saved settings)", &["Yes", "No"]);
-
-        if save_to_file.as_str() == "0" {
-            let mut file = match std::fs::File::create("db_conn.json") {
-                Ok(f) => f,
-                Err(_) => panic!("Error trying to retrieve db_conn.json file")
-            };
-        
-            serde_json::to_writer_pretty(&mut file, &db_info).expect("Error trying to save database info");
-        }
-    } else {
-        let data = std::fs::read_to_string(db_info_fp).expect("Error retrieving file contents of db_conn.json");
-
-        db_info = serde_json::from_str(&data).expect("Error parsing db_conn.json");
-    }
+        let mut load_file = false;
+        let db_info_fp = std::path::Path::new("db_conn.json");
     
-
-    println!("
-        {}
-        [Username] {}
-        [Password] {}
-        [Database] {}
-        [Port]     {}    
-    ",
-        "[ Using the following variables to connect to the database on localhost ]".blue(), 
-        db_info.user.green(), 
-        db_info.password.green(), 
-        db_info.name.green(), 
-        db_info.port.green()
-    );
-
-    let pool = match MySqlPoolOptions::new()
-    .max_connections(2)
-    .connect(format!(
-        "mysql://{}:{}@localhost:{}/{}",
-        db_info.user,
-        db_info.password,
-        db_info.port,
-        db_info.name
-    ).as_str()).await {
-        Ok(pool) => pool,
-        Err(error) => panic!("
-        Error connecting to database: 
-        Make sure you created a database with the name 'newspapers'.
-        {}
-        ", error)
-    };
-
-    println!("{}", "[ Creating table to save news ]".bright_green());
-
-    match sqlx::query(
-        "CALL sys.table_exists(?, 'newspapers', @exists); SELECT @exists"
-    ).bind(db_info.name).execute(&pool).await {
-        Ok(exists) => println!("EXISTS {:?}", exists),
-        Err(error) => println!("ERROR {}", error)
-    }
-
-    match sqlx::query(
-        "CREATE TABLE IF NOT EXISTS news (
-            id MEDIUMINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-            title CHAR(200) NOT NULL,
-            text TEXT,
-            saved_on TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-            posted_on DATE,
-            newspaper_name CHAR(40) NOT NULL
-        )"
-    ).execute(&pool).await {
-        Ok(_) => println!("{}", "[ Created table to save news ]".bright_green()),
-        Err(error) => panic!("{error}")
-    };
-
-    let _ = task::spawn(async move {
-        // Will execute every 20 minutes
-        let mut interval = time::interval(Duration::from_secs(60 * 20));
-        println!("{}", "[ Starting to look for new titles ]".bright_blue());
-
-        loop {
-            interval.tick().await;
-
-            let (
-                clarin_news,
-                infobae_news,
-                lanacion_news,
-                lacapital_news,
-                rosario3_news
-            ) = tokio::join!(
-                newspapers::get_clarin(),
-                newspapers::get_infobae(),
-                newspapers::get_lanacion(),
-                newspapers::get_lacapital(),
-                newspapers::get_rosario3()
-            );
-
-            for journal_new in clarin_news {
-                save_news_to_database(&pool, journal_new, String::from("clarin")).await
-            }
-
-            for journal_new in infobae_news {
-                save_news_to_database(&pool, journal_new, String::from("infobae")).await
-            }
-
-            for journal_new in lanacion_news {
-                save_news_to_database(&pool, journal_new, String::from("la nacion")).await
-            }
-
-            for journal_new in lacapital_news {
-                save_news_to_database(&pool, journal_new, String::from("la capital")).await
-            }
-
-            for journal_new in rosario3_news {
-                save_news_to_database(&pool, journal_new, String::from("rosario3")).await
+        if db_info_fp.exists() {
+            let user_load = menu("There are database connection settings saved, do you want to load them?", &["Yes", "No"]);
+    
+            if user_load.as_str() == "0" {
+                println!("{}", "[ Loading information to connect to database from file ]".green());
+                load_file = true;
             }
         }
-    }).await;
+    
+    
+        let mut db_info = DBInfo {
+            user: String::from(""),
+            password: String::from(""),
+            name: String::from(""),
+            port: String::from("")
+        };
+    
+        if !load_file {
+            db_info.user = menu("Enter your MySQL username", &[]);
+            db_info.password = menu("Enter your MySQL password", &[]);
+            db_info.name = menu("Enter the name of the database you want to use", &[]);
+            db_info.port = menu("Enter the port where the database server is running", &[]);
+    
+            let save_to_file = menu("Do you want to save these connection settings to a file? (This will remove previously saved settings)", &["Yes", "No"]);
+    
+            if save_to_file.as_str() == "0" {
+                let mut file = match std::fs::File::create("db_conn.json") {
+                    Ok(f) => f,
+                    Err(_) => panic!("Error trying to retrieve db_conn.json file")
+                };
+            
+                serde_json::to_writer_pretty(&mut file, &db_info).expect("Error trying to save database info");
+            }
+        } else {
+            let data = std::fs::read_to_string(db_info_fp).expect("Error retrieving file contents of db_conn.json");
+    
+            db_info = serde_json::from_str(&data).expect("Error parsing db_conn.json");
+        }
+        
+    
+        println!("
+            {}
+            [Username] {}
+            [Password] {}
+            [Database] {}
+            [Port]     {}    
+        ",
+            "[ Using the following variables to connect to the database on localhost ]".blue(), 
+            db_info.user.green(), 
+            db_info.password.green(), 
+            db_info.name.green(), 
+            db_info.port.green()
+        );
+    
+        let pool = match MySqlPoolOptions::new()
+        .max_connections(2)
+        .connect(format!(
+            "mysql://{}:{}@localhost:{}/{}",
+            db_info.user,
+            db_info.password,
+            db_info.port,
+            db_info.name
+        ).as_str()).await {
+            Ok(pool) => pool,
+            Err(error) => panic!("
+            Error connecting to database: 
+            Make sure you created a database with the name 'newspapers'.
+            {}
+            ", error)
+        };
+    
+        println!("{}", "[ Creating table to save news ]".bright_green());
+    
+        match sqlx::query(
+            "CALL sys.table_exists(?, 'newspapers', @exists); SELECT @exists"
+        ).bind(db_info.name).execute(&pool).await {
+            Ok(exists) => println!("EXISTS {:?}", exists),
+            Err(error) => println!("ERROR {}", error)
+        }
+    
+        match sqlx::query(
+            "CREATE TABLE IF NOT EXISTS news (
+                id MEDIUMINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                title CHAR(200) NOT NULL,
+                text TEXT,
+                saved_on TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                posted_on DATE,
+                newspaper_name CHAR(40) NOT NULL
+            )"
+        ).execute(&pool).await {
+            Ok(_) => println!("{}", "[ Created table to save news ]".bright_green()),
+            Err(error) => panic!("{error}")
+        };
+
+        let _ = task::spawn(async move {
+            // Will execute every 20 minutes
+            let mut interval = time::interval(Duration::from_secs(60 * 20));
+            println!("{}", "[ Starting to look for new titles ]".bright_blue());
+    
+            loop {
+                interval.tick().await;
+    
+                let (
+                    clarin_news,
+                    infobae_news,
+                    lanacion_news,
+                    lacapital_news,
+                    rosario3_news
+                ) = tokio::join!(
+                    newspapers::get_clarin(),
+                    newspapers::get_infobae(),
+                    newspapers::get_lanacion(),
+                    newspapers::get_lacapital(),
+                    newspapers::get_rosario3()
+                );
+    
+                for journal_new in clarin_news {
+                    save_news_to_database(&pool, journal_new, String::from("clarin")).await
+                }
+    
+                for journal_new in infobae_news {
+                    save_news_to_database(&pool, journal_new, String::from("infobae")).await
+                }
+    
+                for journal_new in lanacion_news {
+                    save_news_to_database(&pool, journal_new, String::from("la nacion")).await
+                }
+    
+                for journal_new in lacapital_news {
+                    save_news_to_database(&pool, journal_new, String::from("la capital")).await
+                }
+    
+                for journal_new in rosario3_news {
+                    save_news_to_database(&pool, journal_new, String::from("rosario3")).await
+                }
+            }
+        }).await;
+    } else {
+        let _ = task::spawn(async move {
+            let mut interval = time::interval(Duration::from_secs(60 * 10));
+    
+            loop {
+                println!("Looking for new titles");
+                interval.tick().await;
+    
+                let (
+                    clarin_news,
+                    rosario3_news
+                ) = tokio::join!(
+                    newspapers::get_clarin(),
+                    newspapers::get_rosario3()
+                );
+
+                print_news("CLARIN", clarin_news);
+                print_news("ROSARIO3", rosario3_news);
+            }
+        }).await;
+    }
+
 
     
 }
